80dd165e21f53635af000622200fd9bc917ef4f5
Raza Rizvi <rrizvi@mirsee.com>
2024-04-19 14:24:18 -0400

    updated

--- scripts/transformer_node.py
+++ scripts/transformer_node.py
@@ -3,9 +3,12 @@ import rospy
 import tf
 from geometry_msgs.msg import PoseStamped
 
+br = tf.TransformBroadcaster()
+
+
 def drone_callback(pose_stamped):
     # Invert the pose
-    br = tf.TransformBroadcaster()
+    # br = tf.TransformBroadcaster()
     inv_translation = (-pose_stamped.pose.position.x, -pose_stamped.pose.position.y, -pose_stamped.pose.position.z)
     inv_rotation = tf.transformations.quaternion_inverse([pose_stamped.pose.orientation.x, pose_stamped.pose.orientation.y, pose_stamped.pose.orientation.z, pose_stamped.pose.orientation.w])
     # Broadcast the new transform
@@ -31,7 +34,7 @@ def drone_callback(pose_stamped):
 
 def hl2_callback(pose_stamped):
     # Invert the pose
-    br = tf.TransformBroadcaster()
+    # br = tf.TransformBroadcaster()
     inv_translation = (-pose_stamped.pose.position.x, -pose_stamped.pose.position.y, -pose_stamped.pose.position.z)
     inv_rotation = tf.transformations.quaternion_inverse([pose_stamped.pose.orientation.x, pose_stamped.pose.orientation.y, pose_stamped.pose.orientation.z, pose_stamped.pose.orientation.w])
     # Broadcast the new transform
@@ -54,16 +57,32 @@ def hl2_callback(pose_stamped):
 
     hl2_pos_pub.publish(inv_pose_stamped)
 
+def world_hl2_callback(pose_stamped):
+    listener = tf.TransformListener()
+    try:
+        # Wait for the transformation from hl2_marker_pose_frame to aruco_marker to become available
+        listener.waitForTransform("aruco_marker", "hl2_marker_pose_frame", rospy.Time.now(), rospy.Duration(4.0))
+        (trans, rot) = listener.lookupTransform("aruco_marker", "hl2_marker_pose_frame", rospy.Time(0))
+        world_to_aruco_trans = (pose_stamped.pose.position.x - trans[0], pose_stamped.pose.position.y - trans[1], pose_stamped.pose.position.z - trans[2])
+        world_to_aruco_rot = tf.transformations.quaternion_multiply(
+            [pose_stamped.pose.orientation.x, pose_stamped.pose.orientation.y, pose_stamped.pose.orientation.z, pose_stamped.pose.orientation.w],
+            tf.transformations.quaternion_inverse(rot))
+        # Optionally, broadcast this transformation
+        br.sendTransform(world_to_aruco_trans,
+                         world_to_aruco_rot,
+                         rospy.Time.now(),
+                         "aruco_marker",
+                         "world")
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+        rospy.logerr("TF exception: %s", e)
+
 if __name__ == '__main__':
     rospy.init_node('camera_pose_publisher')
-    # Create a publisher for PoseStamped
-    drone_pos_pub = rospy.Publisher('/drone/camera_pose_in_marker_frame', PoseStamped, queue_size=10)
     rospy.Subscriber("/aruco_drone/pose", PoseStamped, drone_callback)
-
-    hl2_pos_pub = rospy.Publisher('/hl2/camera_pose_in_marker_frame', PoseStamped, queue_size=10)
     rospy.Subscriber("/aruco_hl2/pose", PoseStamped, hl2_callback)
+    rospy.Subscriber("/Player0/camera/pose", PoseStamped, world_hl2_callback)
 
-    
-    
+    drone_pos_pub = rospy.Publisher('/drone/camera_pose_in_marker_frame', PoseStamped, queue_size=10)
+    hl2_pos_pub = rospy.Publisher('/hl2/camera_pose_in_marker_frame', PoseStamped, queue_size=10)
 
     rospy.spin()
