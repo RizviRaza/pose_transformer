a9f168abc932c3d2787dff6bbf643ddc6e907c29
Raza Rizvi <rrizvi@mirsee.com>
2024-08-06 17:06:27 -0400

    hl2 launch added

--- scripts/new_transformer_node.py
+++ scripts/new_transformer_node.py
@@ -2,11 +2,23 @@
 
 import rospy
 import tf
-from geometry_msgs.msg import PoseStamped, TransformStamped
 import tf.transformations as tf_trans
+from sensor_msgs.msg import CameraInfo
+from geometry_msgs.msg import PoseStamped, TransformStamped
+
 
 # Node to subscribe to the aruco_marker_frame's pose wrt to the base_link of the drone
 # And invert it to publish the drone's base_link pose wrt to the aruco marker
+
+def camera_info_callback(camera_info_msg):
+    # Update the D matrix with the new values
+    camera_info_msg.D = [0.0355633167278393, -0.123584193169489, -0.000250953321849423, -0.000181034148553509, 0.155155222181198]
+    # camera_info_msg.R = [0.0355633167278393, -0.123584193169489, -0.000250953321849423, -0.000181034148553509, 0.155155222181198, -0.000181034148553509, 0.155155222181198, -0.000181034148553509, 0.155155222181198]
+    camera_info_msg.P = [2992.533447265625, 0.0, 1917.8743896484375, 0.0, 0.0, 2994.665771484375, 1038.7142333984375, 0.0, 0.0, 0.0, 1.0, 0.0]
+
+    # Publish the modified CameraInfo message to a new topic
+    hl2_camera_info_pub.publish(camera_info_msg)
+
 
 def pose_callback(msg):
     # Get the pose from the message
@@ -46,5 +58,9 @@ def pose_callback(msg):
 if __name__ == '__main__':
     rospy.init_node('drone_pose_with_respect_to_marker')
     inverted_pose_pub = rospy.Publisher('/inverted_pose', PoseStamped, queue_size=10)
+
+    hl2_camera_info_pub = rospy.Publisher('/Player0/camera/camera_info/updated', CameraInfo, queue_size=10)
+    
+    rospy.Subscriber('/Player0/camera/camera_info', CameraInfo, camera_info_callback) 
     rospy.Subscriber('/aruco_drone/pose', PoseStamped, pose_callback)
     rospy.spin()
